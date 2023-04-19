6beeee34e924b0b81e0e243163ef77c30ad7bb34
Martin <martin.rostejnsky@email.cz>
2023-04-19 21:18:42 +0200

    setup may be working now

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ sudo apt update
 sudo apt install lz4
 sudo apt-get install manpages-dev
 echo 'alias xdd=~/xdd/run.sh' >> ~/.bashrc
-wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/run.sh
-wget -P ~/xdd https://github.com/MartinRostejnsky/xddCompress/raw/main/a.out
-wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/decode.sh
-wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/encode.sh
+wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/run.sh
+wget -P ~/xdd https://github.com/MartinRostejnsky/xddCompress/raw/main/src/a.out
+wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/decode.sh
+wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/encode.sh
 chmod u+x ~/xdd/run.sh
 chmod u+x ~/xdd/a.out
 chmod u+x ~/xdd/decode.sh
--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ $1 = "encode" ]
 then
-    ./encode.sh $2
+    ~/xdd/encode.sh $2
 else
     if [ $1 = "decode" ]
     then
-        ./decode.sh $2
+        ~/xdd/decode.sh $2
     else
         echo $1" is not a valid action"
     fi
