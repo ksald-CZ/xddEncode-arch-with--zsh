21655edbede60284a51160f3c1f63e33a2f67659
Martin Roštejnský <91248036+MartinRostejnsky@users.noreply.github.com>
2023-04-24 20:29:59 +0200

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
+
+SHELL_CONFIG_FILE=
+if [[ $(command -v zsh) ]]; then
+  echo "Detected zsh shell"
+  SHELL_CONFIG_FILE=~/.zshrc
+elif [[ $(command -v bash) ]]; then
+  echo "Detected bash shell"
+  SHELL_CONFIG_FILE=~/.bashrc
+else
+  echo "Unsupported shell"
+  exit 1
+fi
+
 if [ $# -eq 0 ]
 then
   rm -rf ~/.xdd
+  sed '/alias xdd/d' -i $SHELL_CONFIG_FILE
 elif [ $1 = "encode" ]
 then
   ~/.xdd/encode.sh $2
