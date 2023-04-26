9329011f55b7b5f8948ec25313ad9bba517a36aa
Mapetr <info@mapetr.moe>
2023-04-26 11:02:11 +0200

    change name of executable in setup

--- setup.sh
+++ setup.sh
@@ -33,11 +33,11 @@ echo "alias xdd=~/.xdd/run.sh" >> $SHELL_CONFIG_FILE
 
 mkdir -p ~/.xdd
 wget -P ~/.xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/run.sh
-wget -P ~/.xdd https://github.com/MartinRostejnsky/xddCompress/raw/main/src/a.out
+wget -P ~/.xdd https://github.com/MartinRostejnsky/xddCompress/raw/main/src/xddEncode
 wget -P ~/.xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/decode.sh
 wget -P ~/.xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/encode.sh
 chmod u+x ~/.xdd/run.sh
-chmod u+x ~/.xdd/a.out
+chmod u+x ~/.xdd/xddEncode
 chmod u+x ~/.xdd/decode.sh
 chmod u+x ~/.xdd/encode.sh
 
